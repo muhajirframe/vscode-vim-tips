cafaafd3e53ed9420a88ad7f552bc06e0f927964
Muhammad Muhajir <muhajirframe@gmail.com>
2019-01-26 17:29:18 +0800

    Remove unused code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import { Link, StaticQuery } from 'gatsby'
 import { Heading } from 'rebass'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 import AllGifs from '../components/AllGifs'
 
